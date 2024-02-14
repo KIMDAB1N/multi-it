91a5c65a4de38526c703b14f52c6d5f2f39b892d
김다빈 <102177696+KIMDAB1N@users.noreply.github.com>
2024-02-14 20:49:49 +0900

    Delete javascript/240208 directory

--- javascript/240208/10_js_if practice/js/index.js
+++
@@ -1,9 +0,0 @@
-let myNum = Number(prompt('수를 입력해주세요.'));
-
-if (!myNum) {
-    document.write('<h1>오류</h1>');
-} else if (myNum%5==0 || myNum%7==0) {
-    document.write('<h1>통과</h1>');
-} else {
-    document.write('<h1>통과실패</h1>');
-}
